4d72a8ebe375581861be5a3cfae354af00e77291
Wael <wael@Waels-Mac-mini.local>
2022-11-18 13:05:01 +0200

    enhance app

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== `production`
-export const server = dev ? "http://localhost:8000" : "https://notify-gorega.herokuapp.com";
+export const server = dev ? "http://localhost:8000" : "https://notify-2-gorega.vercel.app";
